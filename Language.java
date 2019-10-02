b44c5c6b6b10d4a33236f98f9c369a14df39c212
hadiala1 <hadiala1@students.zhaw.ch>
2019-10-02 10:47:09 +0200

    Add files via upload
    - added new classes
    - added comments

---
+++ Language.java
@@ -0,0 +1,20 @@
+public class Language {
+    private boolean languageEnglish;
+
+    // display welcome Message
+
+    // set language
+
+    // custom message for wining/losing and draw
+
+    // display message for invalid input
+
+    // display message for full board
+
+    // display message for game start
+
+
+
+
+
+}
