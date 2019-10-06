ed2c2d13014f7143cd72b16f9c59b9b3c218fa2a
Sydney Nguyen <nguyesyd@student.zhaw.ch>
2019-10-06 15:30:01 +0200

    Prototype of Logic class

--- src/Logic.java
+++ src/Logic.java
@@ -1,11 +1,17 @@
 public class Logic {
+
+
     public void startGame() {
         // init. a new board
+        Board board = new Board();
 
         // get language input
+        Language language = new Language();
 
         // create new players
-
+        Player x = new Player();
+        Player o = new Player();
+        //initialize inputandoutput
 
         // play game
         playGame();
@@ -15,17 +21,29 @@ public class Logic {
 
     private void playGame() {
 
-        // print empty board
-        /*while (board not full || !(game won))}
-            do
-            switch player
-            get next entry
-            check if game is won/entry is legal
-            print board over method in class board
-          }
-         */
+
+        //method to print the empty board (from class InputAndOutput or from class Board
+        (if isEnglish()){
+            while (!boardIsFull() || !gameWon()) {
+                //either method from InputAndOutput that says players should switch or I do:
+                System.out.println(x.name + ", it is your turn."); //muss das noch ausbauen
+                int neuePosition = scanner.nextInt();
+
+                //if(illegalEntry) do print: illegal entry, pls do another entry
+                //else updateBoard(neuePosition) and print board
+
+            }
+            //if(game won) do print: x.name hat gewonnen
+            //else print Game over.
+        }else{
+            //the same block, just in German
+        }
+
 
         }
+     public void illegalEntry(){
+
+     }
 
 
     }
