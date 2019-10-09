c2090439b757e1237dd4d8d240f6c924ca8d9a96
Kunsang Kundetsang <kuendkun@students.zhaw.ch>
2019-10-09 09:15:52 +0200

    class "Language" - more Javadoc improvements - restructuring of code

--- src/Language.java
+++ src/Language.java
@@ -1,30 +1,51 @@
 /**
  * Diese Klasse beinhalted alle Nachrichten, die nachher an die Klasse InputAndOutput weitergereicht werden.
  * Jede Nachricht ist in den Sprachen Deutsch und Englisch definiert für verschiedene Szenarien.
- * Am anfang wird durch die Klasse Logic der Boolean languageEnglish gesetzt, der nacher dafür sorgt,
+ * Am Anfang wird durch die Klasse Logic der Boolean languageEnglish gesetzt, der nacher dafür sorgt,
  * dass alle Nachrichten in der gewünschten Sprache ausgegeben werden. Falls der Wert vom boolean true ist,
  * wird die Sprache auf Englisch gesetzt und bei false ist sie Deutsch.
+ * @author Küsang Kündetsang
+ * @version 2019_10_09
+ * Modul: Software-Projekt 1, M. Mürner, Dr D. Ehrismann
  */
 
 public class Language {
     private boolean languageEnglish;
 
+    public void setLanguageEnglish(boolean languageEnglish) {
+        this.languageEnglish = languageEnglish;
+    }
+
     /**
      * Diese Methode wird am Anfang des Spiels aufgerufen umd nach dem Sprachwunsch des Benutzers zufragen.
      */
-    public String setLanguageMessage(){
+    public String setLanguageMessage() {
         return "Willkommen! Bitte geben Sie [1] ein für Deutsch. Welcome! Please enter [0] for english.";
     }
 
     /**
      * Diese Methode gibt zurück, dass der nächste Spieler dran ist.
      */
-    public String turnChangesMessage(){
+    public String turnChangesMessage() {
+        String message;
+        if (languageEnglish) {
+            message = "Please switch player.";
+        } else {
+            message = "Bitte Spieler wechseln.";
+        }
+        return message;
+
+    }
+
+    /**
+     * Diese Nachricht wird ausgegeben vor jedem Zug.
+     * */
+    public String getNextMoveMessage() {
         String message;
         if(languageEnglish){
-            message = "Please switch player.";
+            message = "Please enter a number that ranges from 0 to 8";
         }else{
-            message = "Bitte Spieler wechseln.";
+            message = "Bitte eine Zahl von 0 bis 8 eingeben";
         }
         return message;
     }
@@ -32,11 +53,11 @@ public class Language {
     /**
      * Je nach Booleanwert gibt diese Methode eine Gewinn-Message auf Deutsch oder Englisch zurück.
      */
-    public String outcomeWinMessage(){
+    public String outcomeWinMessage() {
         String message;
-        if(languageEnglish){
+        if (languageEnglish) {
             message = "Congratulations, you have won!";
-        }else{
+        } else {
             message = "Gratulation, Sie haben gewonnen!";
         }
         return message;
@@ -45,11 +66,11 @@ public class Language {
     /**
      * Diese Methode gibt eine Message zurück, in Deutsch oder Englisch, im Falle eines Gleichstands.
      */
-    public String outcomeDrawMessage(){
+    public String outcomeDrawMessage() {
         String message;
-        if(languageEnglish){
+        if (languageEnglish) {
             message = "This game ends in a draw.";
-        }else{
+        } else {
             message = "Dieses Spiel endet mit einem Unentschieden.";
         }
         return message;
@@ -58,23 +79,24 @@ public class Language {
     /**
      * Falls ein ungültiger Wert eingegeben wird vom Benutzer, wird diese Nachricht zurückgegeben.
      */
-    public String invalidInputMessage(){
+    public String invalidInputMessage() {
         String message;
-        if(languageEnglish){
+        if (languageEnglish) {
             message = "Please give a valid input.";
-        }else{
-            message = "Bitte geben Sie eine gültige Eingabe.";
+        } else {
+            message = "Bitte geben Sie eine gültige Eingabe ein.";
         }
         return message;
     }
+
     /**
      * Am Ende des Spiels fragt diese Methode, ob ein neues gestarted werden soll.
      */
-    public String gameEndMessage(){
+    public String gameEndMessage() {
         String message;
-        if(languageEnglish){
+        if (languageEnglish) {
             message = "The game has ended, would you like to continue playing? No [1] Yes [0]";
-        }else{
+        } else {
             message = "Das Spiel ist zu ende, möchten Sie weiter spielen? Nein [1] Ja [0]";
         }
         return message;
@@ -83,33 +105,13 @@ public class Language {
     /**
      * Im Falle das der Benutzer nicht mehr weiterspielen möchte, wird am Ende noch eine Dankeschön-Nachricht ausgegeben.
      */
-    public String thankYouMessage(){
-        String message;
-        if(languageEnglish){
-            message = "Thank you very much for playing!";
-        }else{
-            message = "Danke viel Mals fürs spielen!";
-        }
-        return message;
-    }
-
-    public void setLanguageEnglish(boolean languageEnglish) {
-        this.languageEnglish = languageEnglish;
-    }
-
-    public String getNextMoveMessage() {
-        String message;
-        if(languageEnglish){
-            message = "Please inter a number in the range 0 to 8";
-        }else{
-            message = "Bitte eine Zahl von 0 bis 8 eingeben";
-        }
-        return message;
-    }
-
     public String thankYouMessage() {
         String message;
-        message= "Thanks";
+        if (languageEnglish) {
+            message = "Thank you very much for playing!";
+        } else {
+            message = "Danke viel Mals fürs spielen!";
+        }
         return message;
     }
 }
